9ae93298997a66acaa328907b177ecb71c3c11dc
Ryan <asheepingreen@hotmail.com>
2010-11-11 15:11:42 -0400

    trying to fix heroku db issue

--- app/models/recording.rb
+++ app/models/recording.rb
@@ -1,5 +1,5 @@
 class Recording < ActiveRecord::Base
-  attr_accessible :audio_file, :word_id, :alias, :call_id, :short_text
+  attr_accessible :audio_file, :word_id, :alias, :call_id, :short_text, :callcompleted
   
   belongs_to :user
   belongs_to :word
