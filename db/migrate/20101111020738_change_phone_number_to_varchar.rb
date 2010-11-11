e782ff4d1b774ab27a00b5e64006913f0afd358e
Ryan <asheepingreen@hotmail.com>
2010-11-11 14:47:08 -0400

    trying to fix heroku db issue

--- db/migrate/20101111020738_change_phone_number_to_varchar.rb
+++
@@ -1,9 +0,0 @@
-class ChangePhoneNumberToVarchar < ActiveRecord::Migration
-  def self.up
-    change_column :users, :phone_number, :string
-  end
-
-  def self.down
-    change_column :users, :phone_number, :string
-  end
-end
