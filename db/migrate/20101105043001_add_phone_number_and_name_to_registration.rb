c77bc91ea6db21aea4a022316b75c926e69ccdd0
Ryan <asheepingreen@hotmail.com>
2010-11-10 22:25:00 -0400

    what

--- db/migrate/20101105043001_add_phone_number_and_name_to_registration.rb
+++ db/migrate/20101105043001_add_phone_number_and_name_to_registration.rb
@@ -1,7 +1,7 @@
 class AddPhoneNumberAndNameToRegistration < ActiveRecord::Migration
   def self.up
     add_column :users, :name, :string
-    add_column :users, :phone_number, :integer
+    add_column :users, :phone_number, :string
   end
 
   def self.down
